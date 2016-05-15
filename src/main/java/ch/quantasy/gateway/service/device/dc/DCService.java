649b8dc14dd7c6a99940a6956547f585f445d35a
knr1 <knr1@bfh.ch>
2016-05-16 00:31:50 +0200

    .

--- src/main/java/ch/quantasy/gateway/service/device/dc/DCService.java
+++ src/main/java/ch/quantasy/gateway/service/device/dc/DCService.java
@@ -1,14 +1,43 @@
 /*
- * Within this step, a service is using the MQTTCommunication and the 'business-logic'
- * The response of the 'business logic' is promoted to the event topic.
- * The 'business-logic' is invoked via the service and then is self-sustaining.
- * The 'business-logic' sends the result via Callback
- * This way we have a Model-'View'-Presenter (MVP) Where the presenter (the service) is glueing together
- * the Model ('business-logic') and the 'View' (the MQTT-Communication)
- * The service is promoting more status information to the status topic about the underlying 'business-logic'.
+ *   "TiMqWay"
  *
- * This time, an agent is communicating with the service and controls it.
- * This way we delve into the Service based Agent oriented programming
+ *    TiMqWay(tm): A gateway to provide an MQTT-View for the Tinkerforge(tm) world (Tinkerforge-MQTT-Gateway).
+ *
+ *    Copyright (c) 2015 Bern University of Applied Sciences (BFH),
+ *    Research Institute for Security in the Information Society (RISIS), Wireless Communications & Secure Internet of Things (WiCom & SIoT),
+ *    Quellgasse 21, CH-2501 Biel, Switzerland
+ *
+ *    Licensed under Dual License consisting of:
+ *    1. GNU Affero General Public License (AGPL) v3
+ *    and
+ *    2. Commercial license
+ *
+ *
+ *    1. This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *    2. Licensees holding valid commercial licenses for TiMqWay may use this file in
+ *     accordance with the commercial license agreement provided with the
+ *     Software or, alternatively, in accordance with the terms contained in
+ *     a written agreement between you and Bern University of Applied Sciences (BFH),
+ *     Research Institute for Security in the Information Society (RISIS), Wireless Communications & Secure Internet of Things (WiCom & SIoT),
+ *     Quellgasse 21, CH-2501 Biel, Switzerland.
+ *
+ *
+ *     For further information contact <e-mail: reto.koenig@bfh.ch>
+ *
+ *
  */
 package ch.quantasy.gateway.service.device.dc;
 
